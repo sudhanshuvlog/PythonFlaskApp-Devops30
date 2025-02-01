2c0eedcce2afb1243c3d12617f1e600aa25f5480
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2025-02-01 21:33:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    x=10
-    y=20
     @app.route('/')
     def home():
         return 'GeeksForGeeks'
 
     return app
-
-def a():
-    print("test")
-    a()
-def a():
-    print("test")
-    a()
-a()
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=80, debug=True)
