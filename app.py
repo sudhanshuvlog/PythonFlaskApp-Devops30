b068eb92c601cc5bc48762875d63ae4a00b01f8a
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2025-02-02 20:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    def testing():
+        print("hi testing...")
     @app.route('/')
     def home():
+        print("hi hello")
         return 'GeeksForGeeks'
 
     return app
